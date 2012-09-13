7a497c9d99180197b6ffe05285f8acd93826bc2d
d janssen <dajjans4@gmail.com>
2012-09-12 23:21:46 -0400

    reset date to actual arrival date in winnipeg

--- lindsay_countdown_emailer/src/emailer/DateCalculations.java
+++ lindsay_countdown_emailer/src/emailer/DateCalculations.java
@@ -8,7 +8,7 @@ public class DateCalculations {
 	
 	public static LocalDateTime getArrivalDate(){
 		//I arrive in Winnipeg at 12:15 am on Oct 5th EST
-		return new LocalDateTime(2012, 10, 5, 23, 15);
+		return new LocalDateTime(2012, 10, 5, 0, 15);
 	}
 	
 	public static LocalDateTime getCurrentDate(){
--- lindsay_countdown_emailer/src/emailer/Main.java
+++ lindsay_countdown_emailer/src/emailer/Main.java
@@ -1,9 +1,25 @@
 package emailer;
 
+
 public class Main {
 
 	public static void main(String args[]){
-		System.out.print("There are not THAT many days until we see each other");
+		boolean sendEmail = true;
+		System.out.println("There are not THAT many days until we see each other");
+		
+		System.out.println("Seconds until I am in Winnipeg:");
+		while(DateCalculations.getDateDiff() > 0){
+			int secsToWinnipeg = DateCalculations.getDateDiff();
+			System.out.print(secsToWinnipeg + "\r");
+			if(secsToWinnipeg % 86400== 0){
+				if (sendEmail){
+					System.out.println("Whole day less");
+					sendEmail = false;
+				}
+			}else{
+				sendEmail = true;
+			}
+		}
 	}
 	
 }
